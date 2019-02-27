1ec78ae800954d4c67f53934564515e67a9fcd6c
Mehdi Merimi <mehdi.78340@hotmail.fr>
2019-02-27 19:30:38 +0100

    ajout de l'ecriture dans arbo des fichier mais un petit pb demeure

--- arborescence.go
+++ arborescence.go
@@ -1,37 +1,65 @@
 package main
 
-import "io/ioutil"
-import "fmt"
-//import "strings"
-/*
+import (
+
+    "fmt"
+    "io/ioutil"
+    //"syscall"
+)
+import "strings"
+
 func cut(path string) string {
 
    
 
 
-    path=path[:strings.LastIndexAny(path, "/")]
+    path=path[:strings.LastIndexAny(path, "/")+1]
 
 
     return path
 }
-*/
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 func lister(path string){
         entries, err := ioutil.ReadDir(path)
+         //path=strings.TrimRight(path,"/")
 
     if err != nil {
 
         fmt.Println(err)
     }
     for i, entry := range entries {
-
+        /*
     fmt.Println(entry.Name())    // Nom du fichier ("myphoto.jpg")
     fmt.Println(entry.Size())    // Taille en octet (/1024 = Ko)
     fmt.Println(entry.Mode())    // Droits d'écritures "-rw-rw-rw-"
     fmt.Println(entry.ModTime()) // Date de dernière modification
     fmt.Println(entry.IsDir())   // "false" par défaut (car on ne liste pas des "directories" / répertoires)
+    */
     fmt.Println(i)
+    //fmt.Println(path+entry.Name())
+
+    if(!entry.IsDir() ){
+    d1 := []byte("hello")
+
+    err := ioutil.WriteFile(path+entry.Name(), d1, 0644)
+    check(err)
+    fmt.Println(path+entry.Name())
+    }
+    
+    
     if(entry.IsDir()){
-        lister(path+entry.Name()+"/")
+        path =path+entry.Name()+"/"
+
+        lister(path)
+        //fmt.Println(path)
+        path=strings.TrimRight(path,"/")
+        path=strings.TrimRight(path,entry.Name())
+        //fmt.Println(path)
     }
 
 }
@@ -68,8 +96,13 @@ func main() {
     }
     fmt.Println(path)
     */
-    path := "/home/user/Bureau/app/"
-  
+    path := "/home/user/Bureau/R/src/main/java/fr/uvsq/inf103/rogue_like/creature/"
+    /*
+    path2 :="/home/user/Bureau/R/src/main/java/fr/uvsq/inf103/rogue_like/creature/Creature.java"
+       d1 := []byte("hello")
+    err := ioutil.WriteFile(path2, d1, 0644)
+    check(err)
+    */
     lister(path)
     //lister_arbo(path)
 
