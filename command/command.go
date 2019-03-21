55f2b95a46c2d523090457bb5777877ac03a496f
Heisenberk <clement.caumes@orange.fr>
2019-03-21 17:22:48 +0100

    Ebauche squelette app

---
+++ command/command.go
@@ -0,0 +1,5 @@
+package command
+
+func Test() string {
+	return "coucou"
+}
---
+++ goshield.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/Heisenberk/goshield/command"
+
+
+func main() {
+	fmt.Println(command.Test())
+}
